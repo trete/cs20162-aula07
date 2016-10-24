cdde0a35cf5906fe0beaa157f85a0ecb95b64a42
trete <flytrete@hotmail.com>
2016-10-24 11:33:33 -0200

    Update Calendario.java

--- src/main/java/com/github/trete/cs20162/aula07/Calendario.java
+++ src/main/java/com/github/trete/cs20162/aula07/Calendario.java
@@ -1,4 +1,7 @@
-
+/*
+ * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
 package com.github.trete.cs20162.aula07;
 
 import java.time.LocalDate;
